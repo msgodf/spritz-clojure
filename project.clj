6fd71229f0877c4bff285e04d326c2bfc44ee222
Mark Godfrey <msgodf@gmail.com>
2014-11-11 21:09:10 +0000

    Change license and description in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject spritz-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "An implementation of the Spritz cipher in Clojure."
+  :url "https://github.com/msgodf/spritz-clojure"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.0"]]
   :plugins [[cider/cider-nrepl "0.7.0"]
